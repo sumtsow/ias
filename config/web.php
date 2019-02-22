85eff20c509785b31ab2ce94b9869b11241aff2b
Dmytro Sumtsov <sumtsow@gmail.com>
2019-02-23 00:25:11 +0200

    Update

--- config/web.php
+++ config/web.php
@@ -36,7 +36,7 @@ $config = [
                 'class' => 'Swift_SmtpTransport',
                 'host' => 'smtp.meta.ua',  // e.g. smtp.mandrillapp.com or smtp.gmail.com
                 'username' => 'sumtsow@meta.ua',
-                'password' => 'H7h2vj3qq4',
+                'password' => 'tRe;J-%7#32p',
                 'port' => '465', // Port 25 is a very common port too
                 'encryption' => 'ssl', // It is often used, check your provider or mail server specs
             ],            
