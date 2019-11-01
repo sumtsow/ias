977d96a3eb641703e5c0fec25df9c723912ac34d
Dmytro Sumtsov <sumtsow@gmail.com>
2019-11-01 16:47:38 +0200

    Renamed users table name

--- models/User.php
+++ models/User.php
@@ -141,7 +141,7 @@ class User extends ActiveRecord implements IdentityInterface
      */
     public static function tableName()
     {
-        return 'user';
+        return 'users';
     }
 
     /**
